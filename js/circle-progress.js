c2fd4cc3dc51c9a31c603a8259abe8e77cb3dee3
ap983983 <ap983983@gmail.com>
2022-08-27 17:45:30 +0530

    change entire UI

--- js/circle-progress.js
+++ js/circle-progress.js
@@ -1,12 +1,22 @@
-const numb = document.querySelector(".key");
+// const numb = document.querySelector(".key");
 
 
-let counter = 0;
-setInterval(() => {
-  if(counter == 3 ){
-    clearInterval();
-  }else{
-    counter+=1;
-    numb.textContent = counter + "%";
-  }
-}, 10);
+// let counter = 0;
+// setInterval(() => {
+//   if(counter == 3 ){
+//     clearInterval();
+//   }else{
+//     counter+=1;
+//     numb.textContent = counter + "%";
+//   }
+// }, 10);
+
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.text-style', options);
